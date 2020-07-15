59fc7db43f88007b4d6e735686066fc234f1d029
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-07-16 01:16:04 +0200

    Add redist stuff to main dir

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ mkdir /teamspeak
 tar -xjf teamspeak.tar.bz2
 mv teamspeak3-server_linux_amd64/* /teamspeak
 mkdir /teamspeak/{logs,files}
+cp /teamspeak/redist/* /teamspeak/
 chown teamspeak:teamspeak /teamspeak -R
 chmod 400 /teamspeak/*
-chmod 500 /teamspeak/{*.so,ts3server_startscript.sh,files,sql,logs}
+chmod 500 /teamspeak/{*.so,*.so.2,ts3server_startscript.sh,files,sql,logs}
 chmod 700 /teamspeak/{files,logs}/ -R
 rm -rf /teamspeak/{doc,CHANGELOG,serverquerydocs,query_ip_blacklist.txt,query_ip_whitelist.txt}
 echo "TS3 installed!"
