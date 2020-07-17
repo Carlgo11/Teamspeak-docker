3966f454941799bc39d80a3804756a53e34512c7
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-07-17 20:30:12 +0200

    Switch to Alpine

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
-FROM ubuntu
-WORKDIR /tmp
-RUN apt update; apt install curl jq wget -y
-COPY install.sh install.sh
-COPY run.sh run.sh
-RUN chmod +x install.sh
-RUN useradd -Ms /bin/bash teamspeak
-RUN ./install.sh
-USER teamspeak
+FROM alpine
+RUN mkdir /teamspeak
 WORKDIR /teamspeak
-CMD ./run.sh
+
+RUN apk --no-cache add curl jq wget bash libstdc++
+COPY install.sh install.sh
+RUN chmod 500 install.sh
+RUN addgroup -S teamspeak && adduser -s /bin/bash -S teamspeak -G teamspeak
+RUN ./install.sh
+RUN apk del curl jq wget
+
+USER teamspeak
+CMD bash -c "./ts3server_startscript.sh start license_accepted=1 inifile=ts3server.ini; sleep 1; tail -F /teamspeak/logs/ts3server_*.log"
--- install.sh
+++ install.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
 # Download latest ts3 server
-download=`curl -s https://teamspeak.com/versions/server.json | jq -r '.linux.x86_64.mirrors."teamspeak.com"'`
-wget $download -O teamspeak.tar.bz2
+url=$(curl -s https://teamspeak.com/versions/server.json | jq -r '.linux.x86_64.mirrors."teamspeak.com"')
+download=$(echo "$url"| sed -r 's/amd64/alpine/g')
+wget "$download" -O teamspeak.tar.bz2
 
 # Create ts3 dir and unzip downloaded content
-mkdir /teamspeak
-tar -xjf teamspeak.tar.bz2
-mv teamspeak3-server_linux_amd64/* /teamspeak
-mkdir /teamspeak/{logs,files}
-mv /teamspeak/redist/* /teamspeak/
+tar -xjf teamspeak.tar.bz2 --strip-components=1
+mkdir {logs,files}
+mv redist/* ./
 
 # Set permissions and remove unused files
 chown teamspeak:teamspeak /teamspeak -R
-chmod 400 /teamspeak/*
-chmod 500 /teamspeak/{*.so,*.so.2,ts3server_startscript.sh,files,sql,logs}
-chmod 700 /teamspeak/{files,logs}/ -R
-rm -rf /teamspeak/{doc,CHANGELOG,serverquerydocs, redist}
+chmod 400 ./*
+chmod 500 ./{*.so,*.so.2,ts3server_startscript.sh,files,sql,logs}
+chmod 700 ./{files,logs}/ -R
+rm -rf {doc,CHANGELOG,serverquerydocs,redist}
 
 echo "Installation complete!"
