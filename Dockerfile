e35932a7bb991e6d36aea15df301f3ab51c36150
Carlgo11 <Carlgo11@users.noreply.github.com>
2021-02-10 22:39:18 +0100

    Remove bash dependency

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM alpine
 WORKDIR /teamspeak
-RUN apk --force-refresh --no-cache --purge -f -u add curl jq wget bash libstdc++
-RUN adduser -u 1000 -s /bin/bash -S teamspeak teamspeak
+RUN apk --force-refresh --no-cache --purge -f -u add curl jq wget libstdc++
+RUN adduser -u 1000 -S teamspeak teamspeak
 COPY --chown=1000 install.sh install.sh
-RUN chmod 500 install.sh; chown 1000 /teamspeak -R; chmod 700 /teamspeak -R; mkdir files; chmod 700 /teamspeak/files
+RUN chmod 500 install.sh; chown 1000 /teamspeak -R; chmod 700 /teamspeak -R
 USER 1000
-ENTRYPOINT [ "bash", "-c", "./install.sh; ./ts3server license_accepted=1 inifile=ts3server.ini" ]
+ENTRYPOINT [ "sh", "-c", "./install.sh; ls -la files; ./ts3server license_accepted=1 inifile=ts3server.ini" ]
--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+umask 0077
 
 # Download latest ts3 server
 url=$(curl -s https://teamspeak.com/versions/server.json | jq -r '.linux.x86_64.mirrors."teamspeak.com"')
@@ -7,8 +9,9 @@ wget "$download" -O teamspeak.tar.bz2
 
 # Extract ts3server files
 tar -xjf teamspeak.tar.bz2 --strip-components=1
-mv redist/* .
 
-# Clean up files
-rm -rf {teamspeak.tar.bz2doc,serverquerydocs,CHANGELOG,LICENSE,ts3server_minimal_runscript.sh,ts3server_startscript.sh,redist}
-chmod 500 {*.so,*.so.2,ts3server,sql,sql/*/}
+# Set up directory
+[-d "redist" ] || mv redist/* .
+[ -d "files" ] || mkdir files
+for file in teamspeak.tar.bz2 doc serverquerydocs CHANGELOG LICENSE ts3server_minimal_runscript.sh ts3server_startscript.sh redist; do rm -rf $file; done
+for file in *.so *.so.2 ts3server sql sql/*/; do chmod 500 $file; done
