1fa1fb22f795b7b8bbc4077ddfaef89c3e49e363
Carlgo11 <Carlgo11@users.noreply.github.com>
2021-06-13 01:13:06 +0200

    move redist data if dir exists

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ wget "$download" -O teamspeak.tar.bz2
 tar -xjf teamspeak.tar.bz2 --strip-components=1
 
 # Set up directory
-[ -d "redist" ] || mv redist/* .
+[ -d "redist" ] && mv redist/* .
 [ -d "files" ] || mkdir files
 for file in teamspeak.tar.bz2 doc serverquerydocs CHANGELOG LICENSE ts3server_minimal_runscript.sh ts3server_startscript.sh redist; do rm -rf $file; done
 for file in *.so *.so.2 ts3server sql sql/*/; do chmod 500 $file; done
