1f2f49c368b55412201e6bcca260b9d2a9faa801
Carlgo11 <carlgo11@users.noreply.github.com>
2021-09-23 17:10:07 +0200

    Simplify setup of the image

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM alpine
 RUN apk --force-refresh --no-cache --purge -f -u add curl jq libstdc++
 RUN adduser -u 1000 -S teamspeak teamspeak
+RUN mkdir -p /teamspeak/files; chown 1000 /teamspeak/files
 COPY --chown=1000:1000 entrypoint.sh entrypoint.sh
 USER 1000
-ENTRYPOINT [ "sh", "-c", "/entrypoint.sh; cd /teamspeak; ./ts3server license_accepted=1 inifile=ts3server.ini" ]
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["license_accepted=1 inifile=ts3server.ini"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env sh
 umask 0077
-cd /teamspeak/
+
+cd /teamspeak || { echo "tmpfs '/teamspeak' not set" >&2; exit 1; }
 
 # Download latest ts3 server
 url=$(curl -s https://teamspeak.com/versions/server.json | jq -r '.linux.x86_64.mirrors."teamspeak.com"')
-curl -s "$(echo "$url"| sed -r 's/amd64/alpine/g')" -o teamspeak.tar.bz2
+curl -sf "$(echo "$url"| sed -r 's/amd64/alpine/g')" -o teamspeak.tar.bz2 || { echo "Unable to download server" >&2; exit 1; }
 
 # Extract ts3server files
 tar -xjf teamspeak.tar.bz2 --strip-components=1
 
-# Set up directory
+# Set up directories
 [ -d "redist" ] && mv redist/* .
 [ -d "files" ] || mkdir files
 mkdir logs
 
-# Set up database config
+# Set up files
+touch allowlist.txt denylist.txt
 cat <<- EOF >ts3db_mariadb.ini
 		[config]
 		host='${MARIADB_HOST:db}'
@@ -27,8 +29,12 @@ cat <<- EOF >ts3db_mariadb.ini
 EOF
 
 # Remove junk
-for file in teamspeak.tar.bz2 doc serverquerydocs CHANGELOG LICENSE ts3server_minimal_runscript.sh ts3server_startscript.sh redist tsdns; do rm -rf $file; done
+for file in teamspeak.tar.bz2 doc serverquerydocs CHANGELOG LICENSE ts3server_minimal_runscript.sh ts3server_startscript.sh redist tsdns *_sqlite3.so *_postgresql.so sql/*_sqlite* sql/*_postgresql*; do rm -rf "$file"; done
 
 # Set up permissions
 for file in *.so *.so.2 ts3server sql sql/*/ .; do chmod 500 "$file"; done
 chmod 400 ts3db_mariadb.ini
+chmod 700 -R files
+
+# Run the ts3 service
+./ts3server "$@"
